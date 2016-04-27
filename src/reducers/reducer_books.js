9796e705be398ccc686659df7f610339aeabb0ad
Marck <mercercountynj@gmail.com>
2016-04-26 22:03:18 -0400

    npm install and added books reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+// export function by adding export default
+export default function () {
+     return [
+          { title: 'Javascripts The Good Parts' },
+          { title: 'Game of Thrones' },
+          { title: 'To Kill A Mockingbird' },
+          { title: 'Eloquent JavaScript' },
+     ]
+}
