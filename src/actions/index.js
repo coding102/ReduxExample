374c230464d945042ce31972d722e8576e9722b7
Marck <mercercountynj@gmail.com>
2016-05-09 00:06:42 -0400

    adding action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,8 @@
 export function selectBook(book) {
-     console.log('A book was selected', book.title);
+// selectBook is an ActionCreator, it needs to return an action
+// an object wity a type property
+     return {
+          type: 'BOOK_SELECTED',
+          payload: book
+     };
 }
